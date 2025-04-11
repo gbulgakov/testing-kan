12f22c4f6539c5be8e2d811f9f82652b109ddec4
rudanil4 <113044065+rudanil4@users.noreply.github.com>
2025-04-11 14:42:34 +0300

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -68,7 +68,7 @@ import pickle
 def write_results(pkl_path, model_name, emb_name, optim_name,
                   layers, num_epochs, num_params, best_params, 
                   test_accuracies, test_loss, train_times, 
-                  test_times, train_loss_history, val_loss_history):
+                  test_times, train_loss_history, val_loss_history, val_accuracy_history):
     best_params['pkl_path'] = pkl_path
     best_params['layers'] = layers
     best_params['num_epochs'] = num_epochs
@@ -79,6 +79,7 @@ def write_results(pkl_path, model_name, emb_name, optim_name,
     best_params['test_times'] = test_times # т.к. на этапе тестирования нет обучения, то это по сути время инференса
     best_params['train_loss_history'] = train_loss_history
     best_params['val_loss_history'] = val_loss_history
+    best_params['val_accuracy_history'] = val_accuracy_history
 
     if os.path.exists(pkl_path):
         with open(pkl_path, 'rb') as f:
