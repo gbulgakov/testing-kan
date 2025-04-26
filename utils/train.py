74a9cd00007884208ee201b34900b67fdb951cca
rudanil4 <113044065+rudanil4@users.noreply.github.com>
2025-04-27 00:56:32 +0300

    Update train.py
    убрал print

--- utils/train.py
+++ utils/train.py
@@ -111,7 +111,6 @@ def train_epoch(model, device, dataset, base_loss_fn, optimizer, scheduler, mode
         
         # (B, k, n_out) or (B, k) (if regression)
         output = apply_model(batch_data, model)
-        print(output.shape, batch_data['y'].shape)
         loss_value = loss_fn(output, batch_data['y'])
         loss_value.backward()
         optimizer.step()
