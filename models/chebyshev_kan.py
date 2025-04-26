563514445c3898b56dadc3fd878b80e25468009c
rudanil4 <113044065+rudanil4@users.noreply.github.com>
2025-04-27 00:09:18 +0300

    Update chebyshev_kan.py
    поменял inputdim -> in_features, outdim -> out_features для единства с остальными моделями

--- models/chebyshev_kan.py
+++ models/chebyshev_kan.py
@@ -4,14 +4,14 @@ import torch.nn as nn
 
 # This is inspired by Kolmogorov-Arnold Networks but using Chebyshev polynomials instead of splines coefficients
 class ChebyKANLayer(nn.Module):
-    def __init__(self, input_dim, output_dim, degree):
+    def __init__(self, in_features, out_features, degree):
         super(ChebyKANLayer, self).__init__()
-        self.inputdim = input_dim
-        self.outdim = output_dim
+        self.in_features = in_features
+        self.out_features = out_features
         self.degree = degree
 
-        self.cheby_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
-        nn.init.normal_(self.cheby_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
+        self.cheby_coeffs = nn.Parameter(torch.empty(in_features, out_features, degree + 1))
+        nn.init.normal_(self.cheby_coeffs, mean=0.0, std=1 / (in_features * (degree + 1)))
         self.register_buffer("arange", torch.arange(0, degree + 1, 1))
 
     def forward(self, x):
@@ -19,9 +19,9 @@ class ChebyKANLayer(nn.Module):
         # We need to normalize x to [-1, 1] using tanh
         x = torch.clamp(torch.tanh(x), -1 + 1e-6, 1 - 1e-6)  # безопасный tanh + clamp
         # View and repeat input degree + 1 times
-        x = x.view((-1, self.inputdim, 1)).expand(
+        x = x.view((-1, self.in_features, 1)).expand(
             -1, -1, self.degree + 1
-        )  # shape = (batch_size, inputdim, self.degree + 1)
+        )  # shape = (batch_size, in_features, self.degree + 1)
         # Apply acos
         x = x.acos()
         # Multiply by arange [0 .. degree]
@@ -31,8 +31,8 @@ class ChebyKANLayer(nn.Module):
         # Compute the Chebyshev interpolation
         y = torch.einsum(
             "bid,iod->bo", x, self.cheby_coeffs
-        )  # shape = (batch_size, outdim)
-        y = y.view(-1, self.outdim)
+        )  # shape = (batch_size, out_features)
+        y = y.view(-1, self.out_features)
         return y
 
 class ChebyKAN(nn.Module):
@@ -41,8 +41,8 @@ class ChebyKAN(nn.Module):
         self.degree = degree
 
         self.layers = nn.ModuleList([
-            ChebyKANLayer(input_dim, output_dim, degree) 
-            for input_dim, output_dim in zip(layers_hidden[:-1], layers_hidden[1:])
+            ChebyKANLayer(in_features, out_features, degree) 
+            for in_features, out_features in zip(layers_hidden[:-1], layers_hidden[1:])
         ])
 
     def forward(self, x):
