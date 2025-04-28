181bf7d184379d10af72d6db8b0b9295e5d519c0
gbulgakov <113182343+gbulgakov@users.noreply.github.com>
2025-04-29 02:59:37 +0300

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -159,16 +159,14 @@ def get_sweep_config(model_name, emb_name, task_type, sweep_name):
         params.update({
             'mlp_layers' : {'values' : [1, 2, 3, 4]}, # скрытые слои
             'mlp_width' : {'values' : [2 ** i for i in range(11)]},
-            'use_dropout' : {'values' : [True, False],
-                             'probabilities' : [0.7, 0.3] # dropout вероятно нужен
-                            },
+            'use_dropout' : {'values' : [True, False]}
             'dropout' : {'values' : [i / 100 for i in range(0, 55, 5)]}
         })
     elif model_name == 'kan' or model_name == 'batch_norm_kan' or model_name == 'update_grid_kan':
         params.update({
             'kan_layers' : {'values' : [1, 2, 3, 4]},   # скрытые слои
             'kan_width' : {'values' : [2 ** i for i in range(7)]},
-            'grid_size' : {'values' : [3, 30, 2]}
+            'grid_size' : {'values' : [i for i in range(3, 30, 2)]}
         })
     elif model_name == 'small_kan': # легкий KAN
         params.update({
@@ -215,7 +213,7 @@ def get_sweep_config(model_name, emb_name, task_type, sweep_name):
         })
     
     config = {
-        'method' : 'random',
+        'method' : ('bayes' if model_name not in ['mlp_kan', 'kan_mlp'] else 'random'),
         'metric' : metric,
         'parameters' : params,
         'name' : sweep_name
