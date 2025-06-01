049b31a92fd00af77b5827b23db0c438d65d97dd
gbulgakov <bulgakov.ga@phystech.edu>
2025-06-01 23:14:59 +0300

    Обновил размеры батчей

--- project_utils/datasets.py
+++ project_utils/datasets.py
@@ -27,7 +27,8 @@ BATCH_SIZES = {
     'homecredit-default' : 1024,
     'ecom-offers' : 1024, 
     'regression-num-large-0-year' : 1024,
-    'black-friday' : 1024
+    'black-friday' : 1024,
+    'diamond' : 256
 }
 
 # датасет с батчами нужного вида
